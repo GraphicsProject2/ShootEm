2c74579f486ee07dd6fe5aa792ac6a37baf73a20
Viraj Sanjay Muzumdar <v.muzumdar@student.unimelb.edu.au>
2019-10-07 14:14:10 +1100

    Camera Implementation

---
+++ Assets/FollowScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    //the player oject that the camera will follow
+    public GameObject gameobject;
+
+    //position of the camera w.r.t the player
+    public Vector3 _offset= new Vector3(0f, 24f, -5f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _offset = transform.position - gameobject.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        //taken from lab02 camera.
+        Vector3 lookVec = gameobject.transform.position - this.transform.position;
+        this.transform.rotation = Quaternion.LookRotation(lookVec, Vector3.up);
+
+
+        Vector3 pos = gameobject.transform.position + _offset;
+        transform.position = Vector3.Slerp(transform.position, pos, 1.0f);
+        //transform.LookAt(gameObject.transform);
+    }
+}
---
+++ Assets/XAxisSpin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XAxisSpin : MonoBehaviour {
+    
+    public float spinSpeed;
+        	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localRotation *= Quaternion.AngleAxis(Time.deltaTime * spinSpeed, Vector3.right);
+	}
+}
