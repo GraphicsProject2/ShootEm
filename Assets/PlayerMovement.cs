edbab1074c5fd9655e873011099513bd1a63d67f
Mitch Rule <mrule1@student.unimelb.edu.au>
2019-10-07 19:53:02 +1100

    Player and camera movement scripts

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movespeed = 10.0f;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player position
+        Vector3 position = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            position.z += movespeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            position.z -= movespeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            position.x -= movespeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            position.x += movespeed * Time.deltaTime;
+        }
+
+        transform.position = position;
+
+        // Player direction
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.y = 1;
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 relativePosition = targetPosition - mousePosition;
+
+        Quaternion rotation = Quaternion.LookRotation(relativePosition);
+        transform.rotation = rotation;
+    }
+}
