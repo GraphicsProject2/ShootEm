df56b4b1aabb24462d676f96473be746f476d548
Viraj Sanjay Muzumdar <v.muzumdar@student.unimelb.edu.au>
2019-10-12 03:45:50 +1100

    Object generation

---
+++ Assets/terrain/EndlessTerrain.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessTerrain : MonoBehaviour
+{
+    public const float maxViewDist = 450;
+    public Transform viewer;
+
+    public static Vector2 viewerPosition;
+    int chunkSize;
+    int chunksVisibibleViewDist;
+
+    Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
+    List<TerrainChunk> terrainChunkVisibleLastUpdate = new List<TerrainChunk>();
+
+    void Start()
+    {
+        chunkSize = 240;
+        chunksVisibibleViewDist = Mathf.RoundToInt(maxViewDist / chunkSize);
+    }
+
+    void Update()
+    {
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        UpdateVisibleChunks();
+    }
+
+    void UpdateVisibleChunks()
+    {
+        for (int i = 0; i < terrainChunkVisibleLastUpdate.Count; i++)
+        {
+            terrainChunkVisibleLastUpdate[i].SetVisible(false);
+        }
+        int currentChunkCoordX = Mathf.RoundToInt(viewerPosition.x / chunkSize);
+        int currentChunkCoordY = Mathf.RoundToInt(viewerPosition.y / chunkSize);
+        for (int yOffset = -chunksVisibibleViewDist; yOffset <= chunksVisibibleViewDist; yOffset++)
+        {
+            for (int xOffset = -chunksVisibibleViewDist; xOffset <= chunksVisibibleViewDist; xOffset++)
+            {
+                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset);
+                if (terrainChunkDictionary.ContainsKey(viewedChunkCoord))
+                {
+                    terrainChunkDictionary[viewedChunkCoord].UpdateTerrainChunk();
+                    if (terrainChunkDictionary[viewedChunkCoord].isVisible())
+                    {
+                        terrainChunkVisibleLastUpdate.Add(terrainChunkDictionary[viewedChunkCoord]);
+                    }
+                }
+                else
+                {
+                    terrainChunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, transform));
+                }
+
+
+            }
+
+        }
+
+    }
+
+    public class TerrainChunk
+    {
+        GameObject meshObject;
+        Vector2 position;
+        Bounds bounds;
+
+        public TerrainChunk(Vector2 coord, int size, Transform parent)
+        {
+            position = coord * size;
+            bounds = new Bounds(position, Vector2.one * size);
+            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
+
+            meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            meshObject.transform.position = positionV3;
+            meshObject.transform.localScale = Vector3.one * size / 10f;
+            SetVisible(false);
+            meshObject.transform.parent = parent;
+
+        }
+
+        public void UpdateTerrainChunk()
+        {
+            float viewerDistNearestEdge = Mathf.Sqrt(bounds.SqrDistance(viewerPosition));
+            bool visible = viewerDistNearestEdge <= maxViewDist;
+            SetVisible(visible);
+        }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+        }
+
+        public bool isVisible()
+        {
+            return meshObject.activeSelf;
+        }
+    }
+}
---
+++ Assets/terrain/ObjectGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+
+    // The obstacle section to generate obstacles
+    private ObstacleSection obstacleSection;
+
+    /// <summary>
+    /// Awake is used to initialize any variables or game state before the game starts.
+    /// </summary>
+    private void Awake()
+    {
+        Vector3 position = gameObject.transform.position;
+        Vector3 size = gameObject.GetComponent<Renderer>().bounds.size;
+        this.obstacleSection = new ObstacleSection(position, size.x, size.z);
+    }
+
+    /// <summary>
+    /// Updates the obstacles to lie at the new location.
+    /// </summary>
+    public void UpdateObstacles()
+    {
+        Vector3 position = gameObject.transform.position;
+        this.obstacleSection.UpdateCoordinates(position);
+    }
+
+}
