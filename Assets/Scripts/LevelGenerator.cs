4a42b32c44a5003f231f39fdb0f3d1f6965303bd
Viraj Sanjay Muzumdar <v.muzumdar@student.unimelb.edu.au>
2019-10-18 07:45:08 +1100

    3 maps randomly generated

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject tree01;
-    public GameObject tree02;
-    public GameObject rock01;
-    public GameObject rock02;
+    private String object1;
+    private String object2;
+    private String object3;
+    private String object4;
 
-    public int numberOfTree01;
-    public int numberOfTree02;
-    public int numberOfRock01;
-    public int numberOfRock02;
+    public int numObject1;
+    public int numObject2;
+    public int numObject3;
+    public int numObject4;
 
     public GameObject terrain;
 
@@ -23,10 +23,36 @@ public class LevelGenerator : MonoBehaviour
     void Start()
     {
         mc = terrain.GetComponent<MeshCollider>();
+        int random= (int)UnityEngine.Random.Range(1,4);
+        generateLevel(random);
         GenerateGameObjects();
     }
 
-    
+    private void generateLevel(int random)
+    {
+        if (random==1)
+        {
+            object1= "Rock1A";
+            object2 = "Rock1B";
+            object3 = "Rock1C";
+            object4 = "Rock1D";
+        }else if (random==2)
+        {
+            object1 = "tree_a";
+            object2 = "tree_b";
+            object3 = "tree_c";
+            object4 = "tree_d";
+        }
+        else if (random==3)
+        {
+            object1 = "rus_build_2et_01c_low";
+            object2 = "rus_build_5et_03e_low";
+            object3 = "rus_build_5et_05_low";
+            object4 = "rus_build_9et_02b_low";
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
@@ -37,19 +63,19 @@ public class LevelGenerator : MonoBehaviour
 
     private void GenerateGameObjects()
     {
-        GenerateIndividualObject(tree01, numberOfTree01);
-        GenerateIndividualObject(tree02, numberOfTree02);
-        GenerateIndividualObject(rock01, numberOfRock01);
-        GenerateIndividualObject(rock02, numberOfRock02);
+        GenerateIndividualObject(object1, numObject1);
+        GenerateIndividualObject(object2, numObject2);
+        GenerateIndividualObject(object3, numObject3);
+        GenerateIndividualObject(object4, numObject4);
     }
 
-    private void GenerateIndividualObject(GameObject obj, int count)
+    private void GenerateIndividualObject(String obj, int count)
     {
         if (obj == null) return;
 
         for (int i = 0; i < count; i++)
         {
-            GameObject temp = Instantiate(obj);
+            GameObject temp = Instantiate((GameObject)Resources.Load(obj,typeof(GameObject)));
             Vector3 randomPoint = GenerateRandomPoint();
             temp.gameObject.transform.position = new Vector3(randomPoint.x, temp.gameObject.transform.position.y, randomPoint.z);
         }
