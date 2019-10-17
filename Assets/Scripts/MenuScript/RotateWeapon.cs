7655922c4bece7b9795d9a663b5a2760a7756b48
Wong Jen Pin <wongjenpin@gmail.com>
2019-10-18 05:33:46 +1100

    Added Menu Scene

---
+++ Assets/Scripts/MenuScript/RotateWeapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWeapon : MonoBehaviour
+{
+    public float rotationSpeed;
+    private float rotationZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rotationZ = transform.localEulerAngles.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.rotationZ += rotationSpeed * Time.deltaTime;
+        this.rotationZ %= 360;
+        transform.localEulerAngles = new Vector3(0.0f, 0.0f, rotationZ);
+    }
+}
