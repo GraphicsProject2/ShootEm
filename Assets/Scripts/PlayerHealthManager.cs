f5070739dee658c1627cb6858ecb387b4547c722
DanClark4 <dclarke4@student.unimelb.edu.au>
2019-10-17 19:18:10 +1100

    Fixed Sniper, Added Health Text(Not currently taking damage on player)

--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthManager : MonoBehaviour
 {
+
+    public Text healthDisplay;
+    public int playerHealth;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Set the players health by default
+        playerHealth = 100;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Display health 
+        this.healthDisplay.text = "Health: " + playerHealth;
     }
 }
