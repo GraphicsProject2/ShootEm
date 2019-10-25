eb4da3b357c626e5b9e70dd5b0be0e755bf96f9e
rulemitch <mrule1@student.unimelb.edu.au>
2019-10-25 13:24:45 +1100

    fixed merge conflicts, player/enemy movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,47 +6,44 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movespeed = 10.0f;
     //public Transform target;
-    public GameObject player;
+    //public GameObject player;
     public float sensitivity;
-
-    private float xLoc;
-    private float zLoc;
-    private float yLoc;
-
-    //private bool inObj;
+    public Rigidbody player;
 
     // Start is called before the first frame update
     void Start()
     {
         // Initalise the player looking foward
-        player.transform.LookAt(transform.forward);
+        //player.transform.LookAt(transform.forward);
+        player = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Player position
-        Vector3 position = transform.position;
+        //Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+        //// Player position
+        ////Vector3 position = transform.position;
 
-        if (Input.GetKey("w"))
-        {
-            position.z += movespeed * Time.deltaTime;
-        }
-        if (Input.GetKey("s"))
-        {
-            position.z -= movespeed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            position.x -= movespeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            position.x += movespeed * Time.deltaTime;
-        }
 
-        transform.position = position;
+        //if (Input.GetKey("w"))
+        //{
+        //    movement.z += movespeed;
+        //}
+        //if (Input.GetKey("s"))
+        //{
+        //    movement.z -= movespeed;
+        //}
+        //if (Input.GetKey("a"))
+        //{
+        //    movement.x -= movespeed;
+        //}
+        //if (Input.GetKey("d"))
+        //{
+        //    movement.x += movespeed;
+        //}
 
+        //player.velocity += movement;
 
         // Adapted from Lab 8 solutions but is being utilised to control the direction of a character
         Vector2 mouseScreenPos = Input.mousePosition;
@@ -65,7 +62,51 @@ public class PlayerMovement : MonoBehaviour
 
         // Set the player to look at the point of intercetion
         player.transform.LookAt(new Vector4(facepos.x, player.transform.forward.y, facepos.z));
-        
+
+        /*
+        // Player direction
+        //Vector3 mousePosition = Input.mousePosition;
+        //mousePosition.y = 1
+        // Use sensitivity inorder to change the rate the weapon moves
+        ///xLoc = sensitivity * (mousePosition.x - (Screen.width - (Screen.width / 2)));
+        / zLoc = sensitivity * (mousePosition.z - (Screen.height - (Screen.height / 2
+        // Set the weapon to be faceing foward
+        //player.transform.rotation.eulerAngles
+        //Debug.Log(new Vector3(xLoc, this.transform.forward.y, zLoc
+       // player.transform.LookAt(new Vector3(xLoc, this.transform.forward.y , 
+        Vector3 targetPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 relativePosition = targetPosition - mousePosition;
+        Debug.Log(mousePosition);
+        Quaternion rotation = Quaternion.LookRotation(relativePosition);
+        transform.rotation = rotation;
+        */
     }
 
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+        // Player position
+        //Vector3 position = transform.position;
+
+
+        if (Input.GetKey("w"))
+        {
+            movement.z += movespeed;
+        }
+        if (Input.GetKey("s"))
+        {
+            movement.z -= movespeed;
+        }
+        if (Input.GetKey("a"))
+        {
+            movement.x -= movespeed;
+        }
+        if (Input.GetKey("d"))
+        {
+            movement.x += movespeed;
+        }
+
+        player.velocity = movement;
+        Debug.Log(player.position);
+    }
 }
